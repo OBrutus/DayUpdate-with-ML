a32720a92c94a788905634f9229e408e1be53303
OBrutus <vaishnavani7@gmail.com>
2022-02-21 23:12:59 +0530

    ➕ Added gktoday related changes, especially title

--- index.js
+++ index.js
@@ -3,31 +3,28 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 const json2csv = require('json2csv').Parser;
 
-const movie = 'https://www.imdb.com/title/tt1877830/?ref_=nv_sr_srsg_0';
+const url = 'https://www.gktoday.in/current-affairs/page/2/';
 
 (async () => {
-    const imdb_data = [];
+    const collection = [];
     
     const response = await request({
-        uri: movie,
-        headers: {
-            'accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-            'accept-encoding': 'gzip, deflate, br',
-            'accept-language': 'en-US,en;q=0.9,hi;q=0.8'
-        },
-        gzip: true
+        uri: url,
     });
 
-    console.log(response);
-    fs.writeFileSync('index.html', response);
     const $ = cheerio.load(response);
-    const title = $('div[class="TitleBlock__TitleContainer-sc-1nlhx7j-1 jxsVNt"] > h1').innerText;
-    const rating = $('div[class="TrendingButton__TrendingScore-sc-bb3vt8-1 efbXIW"]').innerText;
-    const summary = $('span[class="GenresAndPlot__TextContainerBreakpointXS_TO_M-sc-cum89p-0 kHlJyu"]').innerText;
+    const title = $('h1[id="list"] > a');
 
-    imdb_data.push({ title, rating, summary });
-    console.log(imdb_data);
+    for (const node of title) {
+        let actualTitle = node.children[0].data;
+        collection.push(actualTitle);
+    }
 
-    const csv = (new json2csv()).parse(imdb_data);
-    fs.writeFileSync('imdb_data.csv', csv);
+    console.log('data is');
+    console.log(collection);
+
+    fs.writeFileSync('collection.json', collection);
+
+    const csv = (new json2csv()).parse(collection);
+    fs.writeFileSync('collection.csv', csv);
 })();
