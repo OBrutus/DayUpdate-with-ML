18548536e056a3519ffe2edad9dc50037f2b4ed6
OBrutus <vaishnavani7@gmail.com>
2022-02-22 18:31:55 +0530

    Unfinished changes

--- index.js
+++ index.js
@@ -3,26 +3,41 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 const json2csv = require('json2csv').Parser;
 
-const url = 'https://www.gktoday.in/current-affairs/page/2/';
+let currentPage = null;
+const lastPage = 3;
+const hasNextPage = () => currentPage < lastPage;
+
+async function getResponse(...header) {
+    await request(header);
+}
+
+function nextUri() {
+    currentPage = currentPage + 1 ?? 1;
+    return `https://www.gktoday.in/current-affairs/page/${currentPage}/`;
+}
+
+function generateHeader() {
+    return {
+        uri: nextUri(),
+    };
+}
 
 (async () => {
     const collection = [];
     
-    const response = await request({
-        uri: url,
-    });
-
-    const $ = cheerio.load(response);
-    const title = $('h1[id="list"] > a');
-
-    for (const node of title) {
-        let actualTitle = node.children[0].data;
-        collection.push({title: actualTitle});
+    while (hasNextPage()) {
+        const response = getResponse(generateHeader());
+        const $ = cheerio.load(response);
+        const title = $('h1[id="list"] > a');
+        
+        for (const node of title) {
+            let actualTitle = node.children[0].data;
+            collection.push(actualTitle);
+        }
+        console.log(`data at page ${currentPage} is ${collection}`);
+        console.log(collection);
     }
 
     console.log('data is');
     console.log(collection);
-
-    const csv = (new json2csv()).parse(collection);
-    fs.writeFileSync('collection.csv', csv);
 })();
