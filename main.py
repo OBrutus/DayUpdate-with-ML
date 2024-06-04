31c026a50b79e27110eb21710ff3d61582add509
VaishnaV <aniket.v@56secure.com>
2024-06-04 20:18:39 +0530

    Refactored

--- main.py
+++ main.py
@@ -4,48 +4,48 @@ from urllib.request import urlopen
 import sys
 import master
 import os
-#import requests
+
 
 def init():
-	print('Hii '+os.getlogin() +' opening .... ')
-	if sys.argv.__len__() > 1 :
-		firstOption=sys.argv[1]
-		if firstOption=='-c' or firstOption=='-C':
-			print('You sre in customized mode ...')
-			print('This Customisation last only uptill current session.')
-			master.config.verboseCustomize()
-		else:
-			print('Invalid parameter specified')
+    print('Hii '+os.getlogin() + ' opening .... ')
+    if sys.argv.__len__() > 1:
+        first_option = sys.argv[1]
+        if first_option == '-c' or first_option == '-C':
+            print('You sre in customized mode ...')
+            print('This Customisation last only uptill current session.')
+            master.config.verboseCustomize()
+        else:
+            print('Invalid parameter specified')
 
 
-#do not prompt for next jst do next
-
+# do not prompt for next jst do next
 
 
 def main():
-	news_url="http://news.google.com/news/rss"
-	client=urlopen(news_url)
-	page=client.read()
-	client.close()
+    news_url = "http://news.google.com/news/rss"
+    client = urlopen(news_url)
+    page = client.read()
+    client.close()
 
-	soup_page=BeautifulSoup(page, "xml")
-	news_list=soup_page.findAll("item")
+    soup_page = BeautifulSoup(page, "html.parser")
+    news_list = soup_page.findAll("item")
 
-	for news in news_list:
-		text=news.title.text
-		text+='\n'
-		f=open('news.txt','w')
-		f.write(text)
-		master.printNews(news)
-		if(master.config.autoPlay <= 0):
-			ch=input("Continue / GetMoreInfo / Exit [default: Continue] \n[c, i, e] : ")
-			if  ch=='e':
-				break
-			elif ch=='i':
-				master.getMoreInfo(news)
+    for news in news_list:
+        text = news.title.text
+        text += '\n'
+        f = open('news.txt', 'w')
+        f.write(text)
+        master.printNews(news)
+        if (master.config.autoPlay <= 0):
+            ch = input(
+                "Continue / GetMoreInfo / Exit [default: Continue] \n[c, i, e] : ")
+            if ch == 'e':
+                break
+            elif ch == 'i':
+                master.getMoreInfo(news)
 
 
-if __name__=='__main__':
-	init()
-	main()
-	print('\n\n'+'*'*5+'Done'+'*'*5)
+if __name__ == '__main__':
+    init()
+    main()
+    print('\n\n'+'*'*5+'Done'+'*'*5)
--- master.py
+++ master.py
@@ -2,28 +2,30 @@ import time
 import analysis
 from custom import Custom
 
-config=Custom()
+config = Custom()
+
 
 def getMoreInfo(news):
-    eof=config.startOfLine+'\n'
-    print('source: '+str(news.source.text), end=eof)
-    print('link : '+str(news.source.link), end=eof)
+    EOF = config.startOfLine+'\n'
+    print('source: '+str(news.source.text), end=EOF)
+    print('link : '+str(news.source.link), end=EOF)
+
 
 def printNews(news):
-    eof=config.startOfLine+'\n'
+    EOF = config.startOfLine+'\n'
 
-    print('autopla=',config.autoPlay)
+    print('autopla=', config.autoPlay)
 
-    print(config.startOfLine+news.title.text, end=eof)
-    print(news.pubDate.text, end=eof)
+    print(config.startOfLine+news.title.text, end=EOF)
+    print(news.pubDate.text, end=EOF)
 
     if config.detailed:
         getMoreInfo(news)
-    
+
     if config.immidiateAnalysis:
         analysis.analysisInfo(news.title.text)
 
     if config.autoPlay > 0:
         time.sleep(config.autoPlay)
-    
+
     print(config.endOfNewsDelmiter)
